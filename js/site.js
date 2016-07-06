a76c1addbd0ce871cecf8115ae0b64a6281fe607
AA-124 <alyssa.anand@gmail.com>
2016-07-06 11:26:23 -0700

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,57 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("nav")[0].classList.toggle("responsive");
+	
+}
+
+
+$(document).ready(function() {
+$('a[href*=#]').each(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+&amp;amp;&amp;amp; location.hostname == this.hostname
+&amp;amp;&amp;amp; this.hash.replace(/#/,'') ) {
+var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+if ($target) {
+var targetOffset = $target.offset().top;
+$(this).click(function() {
+$("#nav li a").removeClass("active");
+$(this).addClass('active');
+$('html, body').animate({scrollTop: targetOffset}, 1000);
+return false;
+});
+}
+}
+});
+
+});
+
+
+$(document).ready(function() {
+  $('a[href*=#]').each(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname
+    && this.hash.replace(/#/,'') ) {
+      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+       if ($target) {
+         var targetOffset = $target.offset().top;
+		 
+		 
+		 
+		 
+  $(this).click(function() {
+ $("#nav li a").removeClass("active");
+ $(this).addClass('active');
+           $('html, body').animate({scrollTop: targetOffset}, 1000);
+           return false;
+         });
+      }
+    }
+  });
+
+});
+
+
+
+$('html, body').animate({scrollTop: targetOffset}, 1000);
